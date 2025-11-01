adb21a78d086ad2f7dd8f33a57fd2db796db617d
Mustak <gmustak666@gmail.com>
2025-11-01 12:48:47 +0530

    changes in Policy

--- src/main/java/com/claimsprocessingplatform/processingplatform/controller/PolicyController.java
+++ src/main/java/com/claimsprocessingplatform/processingplatform/controller/PolicyController.java
@@ -1,14 +1,13 @@
 package com.claimsprocessingplatform.processingplatform.controller;
 
 import com.claimsprocessingplatform.processingplatform.dto.PolicyClaimDto;
+import com.claimsprocessingplatform.processingplatform.dto.PolicyClaimResponceDto;
 import com.claimsprocessingplatform.processingplatform.service.PolicyClaimService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/policies")
@@ -27,15 +26,16 @@ public class PolicyController {
         return ResponseEntity.ok("Policy created successfully");
     }
     @GetMapping("/getAllClaims")
-    public  ResponseEntity<String> getallClaims(){
-    	 this.policyService.getAllClaims();
-         return ResponseEntity.ok("Policy fetched successfully");
+    public  ResponseEntity<List<PolicyClaimResponceDto>> getallClaims(){
+    	 List<PolicyClaimResponceDto> policyClaimResponceDtos = this.policyService.getAllClaims();
+         return ResponseEntity.ok(policyClaimResponceDtos);
     }
     
-    @GetMapping("/getClaimsById")
-    public  ResponseEntity<String> getClaimsByid(@Path ){
-    	 this.policyService.getAllClaims();
-         return ResponseEntity.ok("Policy fetched successfully");
+    @GetMapping("/getClaimsById/{id}")
+    public  ResponseEntity<PolicyClaimResponceDto> getClaimsByid(@PathVariable String ClaimId ){
+    	 PolicyClaimResponceDto policyClaimResponceDto = this.policyService.getClaimsByid(ClaimId)
+                 .orElseThrow(() -> new IllegalArgumentException("Policy claim not found with ID: " + ClaimId));
+         return ResponseEntity.ok(policyClaimResponceDto);
     }
 
 
--- src/main/java/com/claimsprocessingplatform/processingplatform/model/User.java
+++ src/main/java/com/claimsprocessingplatform/processingplatform/model/User.java
@@ -1,5 +1,19 @@
 package com.claimsprocessingplatform.processingplatform.model;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String email;
+    private String fullName;
 }
